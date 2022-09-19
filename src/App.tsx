fbf49e238c6e0dc3da406b69f6b59d4961916af2
Yan Latyshev <yan.latyshev@gmail.com>
2022-09-19 23:26:47 +0300

    configure and add Redux with TS to application

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import { useAppDispatch, useAppSelector } from './hooks/hooks';
+import { increment } from './store/reducers/UserSlice';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
+
+  const value = useAppSelector(state => state.value.value);
+  const dispatch = useAppDispatch();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <p>TS Template</p>
+      <div><button onClick={() => dispatch(increment())}>+1 к счастью</button><p>{value}</p></div>
     </div>
   );
 }
