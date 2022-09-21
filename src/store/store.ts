28f5a8a393278b4e9ef2495439ae580b10aab574
Yan Latyshev <yan.latyshev@gmail.com>
2022-09-21 13:17:09 +0300

    added a bit more semicolons

--- src/store/store.ts
+++ src/store/store.ts
@@ -6,13 +6,11 @@ const rootReducer = combineReducers({
   value : userReducer
 })
 
-
 export const store = configureStore({
   reducer: rootReducer
     
 })
 
 
-export type RootState = ReturnType<typeof store.getState>
-
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
