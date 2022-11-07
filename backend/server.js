2d84ac59f5940d2ed968542f41035c0ed3fa01ee
Yan Latyshev <yan.latyshev@gmail.com>
2022-11-07 14:29:01 +0200

    exploring node and added express,added scripts itc...

---
+++ backend/server.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3003;
+
+app.listen(PORT, () => { console.log(`all right  ${PORT}`) });
