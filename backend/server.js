bc036fa3f79f03bfa3af6e5109af8000e42ab6c8
Yan Latyshev <yan.latyshev@gmail.com>
2022-11-08 23:08:02 +0200

    test

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,22 @@
-import * as dotenv from "dotenv";
-import * as path from "path";
 import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
+const port = process.env.PORT || 5000;
 
 const app = express();
-const PORT = process.env.PORT || 3003;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/build")));
+
   app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "../", "frontend", "build", "index.html")));
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
 }
 
-app.get("/test", (req, res) => {
-  res.send("Server work");
-});
-
-app.listen(PORT, () => {
-  console.log(`all right  ${PORT}`);
-});
+app.listen(port, () => console.log(`Server started on port ${port}!`));
