c1b4f0093125443de113be304eb3f3dc42c017e7
Yan Latyshev <yan.latyshev@gmail.com>
2022-11-08 22:13:48 +0200

    dirname

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3003;
 
+const __dirname = path.dirname(__filename);
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("frontend/build"));
   app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html")));
