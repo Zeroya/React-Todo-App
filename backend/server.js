6bd2deb1badfb189df8cec6748dc78c1a694f4aa
Yan Latyshev <yan.latyshev@gmail.com>
2022-11-08 17:12:57 +0200

    logger

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,10 @@ if (process.env.NODE_ENV === "production") {
   app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html")));
 }
 
+app.get("/", (req, res) => {
+  res.send("Server work");
+});
+
 app.listen(PORT, () => {
   console.log(`all right  ${PORT}`);
 });
