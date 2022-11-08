162537a6470e5858e5c918627eee338272e3f5a2
Yan Latyshev <yan.latyshev@gmail.com>
2022-11-08 23:42:10 +0200

    test

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
 import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 const port = process.env.PORT || 5000;
 
 const app = express();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "../", "frontend", "build", "index.html")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend/build")));
+
+  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html")));
 } else {
   app.get("/", (req, res) => res.send("Please set to production"));
 }
