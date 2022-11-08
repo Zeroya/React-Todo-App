b69f788e230068e8cf9667b683a9b0f07b51bbeb
Yan Latyshev <yan.latyshev@gmail.com>
2022-11-09 00:31:15 +0200

    test

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,11 @@
 import express from "express";
-import dotenv from "dotenv";
-import path from "path";
-import { fileURLToPath } from "url";
+import * as dotenv from "dotenv";
+import * as path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3003;
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "frontend/build")));
