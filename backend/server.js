3da30911aa92bbfda746eb8981baf5bce1681e83
Yan Latyshev <yan.latyshev@gmail.com>
2022-11-09 15:35:00 +0200

    test

--- backend/server.js
+++ backend/server.js
@@ -5,18 +5,19 @@ import * as path from "path";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3003;
 
 const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/build")));
-  // app.get("*", (req, res) => res.sendFile(path.resolve(__dirname + "/../frontend/build/index.html")));
+  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname + "/../frontend/build/index.html")));
 }
 
 app.get("/", (req, res) => {
   res.send("Home Page");
 });
+
+const PORT = process.env.PORT || 3003;
 
 app.listen(PORT, () => {
   console.log(`all right ${PORT}`);
