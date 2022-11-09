0b910c9e64f86c6a6be2d5b37b811d57b53b2739
Yan Latyshev <yan.latyshev@gmail.com>
2022-11-09 12:54:44 +0200

    test

--- backend/server.js
+++ backend/server.js
@@ -10,8 +10,8 @@ const PORT = process.env.PORT || 3003;
 const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("/frontend/build"));
-  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "/frontend/build/index.html")));
+  app.use(express.static("/../frontend/build"));
+  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "/../frontend/build/index.html")));
 }
 
 app.get("/", (req, res) => {
