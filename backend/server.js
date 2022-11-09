0741885bfe8b4d9a03d7c67a33fe22e978f26ee7
Yan Latyshev <yan.latyshev@gmail.com>
2022-11-09 12:00:59 +0200

    test

--- backend/server.js
+++ backend/server.js
@@ -12,12 +12,12 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
-  //app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html")));
+  app.use(express.static("/frontend/build"));
+  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "/frontend/build/index.html")));
 }
 
-app.get("/test", (req, res) => {
-  res.send("Server work");
+app.get("/", (req, res) => {
+  res.send("Home Page");
 });
 
 app.listen(PORT, () => {
