9824d94a665d68cdadff0ab976c46f137ede0df5
Yan Latyshev <yan.latyshev@gmail.com>
2022-12-15 20:39:36 +0200

    small changes

--- frontend/src/constants/constants.ts
+++ frontend/src/constants/constants.ts
@@ -1,4 +1,3 @@
-export const BASE_URL = "http://localhost:3004";
-// https://mern-todo-app-server.onrender.com
+export const BASE_URL = "https://mern-todo-app-server.onrender.com";
 export const HEADERS = { "Content-Type": "application/json" };
 export const CREDENTIALS = { withCredentials: true };
